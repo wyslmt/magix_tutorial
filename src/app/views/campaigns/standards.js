f845c63eeb30d72a7db70de85b9e230f97e7d6a3
Alan <wyslmt123@gmail.com>
2015-10-17 14:30:19 +0800

    step_3

---
+++ src/app/views/campaigns/standards.js
@@ -0,0 +1,22 @@
+﻿KISSY.add("app/views/campaigns/standards", function (S, View, IO, Mustache) {
+    return View.extend({
+        render: function () {
+            var me = this;
+            IO({
+                url: 'api/list.json',
+                dataType: 'json',
+                success: function (data) {
+                    var html = Mustache.to_html(me.tmpl, {
+                        list: data
+                    });
+                    me.setHTML(me.id, html);
+                },
+                error: function (xhr, msg) {
+                    me.setHTML(me.id, msg); //出错时，直接显示错误
+                }
+            });
+        }
+    });
+}, {
+    requires: ["magix/view", "ajax", "app/common/mustache"]
+});
--- src/app/views/default.js
+++ src/app/views/default.js
@@ -1,9 +1,28 @@
-﻿KISSY.add("app/views/default", function (S, View) {
+﻿KISSY.add("app/views/default", function (S, View, VOM) {
     return View.extend({
-        render: function () {
-            this.setHTML(this.id, this.tmpl);
+        init: function () {
+            this.observeLocation({
+                path: true
+            });
+        },
+        render: function (e) {
+            if (!e) {
+                this.setHTML(this.id, this.tmpl);
+            }
+            this.mountMainFrame();
+        },
+        mountMainFrame: function () {
+            var path = this.location.path;
+            var pns = path.split('/');
+            pns.shift();
+            if (pns[0] == 'home') {
+                pns.push('index'); //特殊处理home
+            }
+            var viewPath = 'app/views/' + pns.join('/');
+            var vframe = VOM.get('magix_vf_main');
+            vframe.mountView(viewPath);
         }
     });
 }, {
-    requires: ['magix/view']
+    requires: ['magix/view', 'magix/vom']
 });
